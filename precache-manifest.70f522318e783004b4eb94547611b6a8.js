16efc7e07ac9c1639470a7882618ccdeff7ea31e
Avisrara74 <avisrara74@gmail.com>
2020-07-09 16:22:46 +0500

    Updates

---
+++ precache-manifest.70f522318e783004b4eb94547611b6a8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b0c978f450b2534ad909f065e495f3",
+    "url": "/jm_blog/index.html"
+  },
+  {
+    "revision": "2e23b4d9c7375e184fcf",
+    "url": "/jm_blog/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "287f0b57ce7da35d2531",
+    "url": "/jm_blog/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2e23b4d9c7375e184fcf",
+    "url": "/jm_blog/static/js/2.de542e37.chunk.js"
+  },
+  {
+    "revision": "32ac14f0e3d33fba5653459742f2759c",
+    "url": "/jm_blog/static/js/2.de542e37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "287f0b57ce7da35d2531",
+    "url": "/jm_blog/static/js/main.6151ad72.chunk.js"
+  },
+  {
+    "revision": "f0823c4b862dce00087d",
+    "url": "/jm_blog/static/js/runtime-main.f64e187d.js"
+  },
+  {
+    "revision": "248977461827377e533377c5327126fe",
+    "url": "/jm_blog/static/media/like-active.24897746.svg"
+  },
+  {
+    "revision": "d17fcd0b93ec5896bd0de49f03bdcb08",
+    "url": "/jm_blog/static/media/like-disabled.d17fcd0b.svg"
+  },
+  {
+    "revision": "1197b2932237119bb89d3db10c83b2aa",
+    "url": "/jm_blog/static/media/user-default-img.1197b293.svg"
+  }
+]);
