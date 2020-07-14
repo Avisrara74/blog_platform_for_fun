67465f7415a59c1ec1981869f638efb58a421bca
Avisrara74 <avisrara74@gmail.com>
2020-07-14 10:41:08 +0500

    Updates

--- precache-manifest.d5eb91bac9fe84dbafdcf7feed30d5b0.js
+++ precache-manifest.d5eb91bac9fe84dbafdcf7feed30d5b0.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f6f2ca5f0fac9e294d7246322a73be8",
+    "revision": "af3416ac39e85cc9f268a09b837a5ea1",
     "url": "/jm_blog/index.html"
   },
   {
-    "revision": "d19c9f0797ace17bdbac",
+    "revision": "8d7ede2140342beaeeff",
     "url": "/jm_blog/static/css/2.3cb76441.chunk.css"
   },
   {
-    "revision": "d0e2ecafd48324a15324",
+    "revision": "b7588e5d65391a1a37b0",
     "url": "/jm_blog/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "d19c9f0797ace17bdbac",
-    "url": "/jm_blog/static/js/2.e89355cd.chunk.js"
+    "revision": "8d7ede2140342beaeeff",
+    "url": "/jm_blog/static/js/2.9ba0f858.chunk.js"
   },
   {
     "revision": "32ac14f0e3d33fba5653459742f2759c",
-    "url": "/jm_blog/static/js/2.e89355cd.chunk.js.LICENSE.txt"
+    "url": "/jm_blog/static/js/2.9ba0f858.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d0e2ecafd48324a15324",
-    "url": "/jm_blog/static/js/main.3326d98f.chunk.js"
+    "revision": "b7588e5d65391a1a37b0",
+    "url": "/jm_blog/static/js/main.a9ea5454.chunk.js"
   },
   {
     "revision": "f0823c4b862dce00087d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jm_blog/precache-manifest.ae4f121e30a6f5169868347329a00448.js"
+  "/jm_blog/precache-manifest.d5eb91bac9fe84dbafdcf7feed30d5b0.js"
 );
 
 self.addEventListener('message', (event) => {
